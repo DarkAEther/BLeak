5199b5ed9438bbbf0a20b01a5ee2c301b5cab217
DarkAEther <30438425+DarkAEther@users.noreply.github.com>
2020-03-06 16:48:32 +0530

    Sample parallelism using worker_threads for graph DFS summation

--- ts_practice/basic_ts.ts
+++
@@ -1,9 +0,0 @@
-interface Person{
-    fname: string;
-    lname: string;
-}
-function greeter(name :Person){
-    return "Hello, " + name.fname + " lname " + name.lname;
-}
-let user = {"fname":"Random","lname":"Person"};
-console.log(greeter(user));
